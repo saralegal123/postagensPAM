39ac4e9d7cba8e92bb780d97e86abdec13285e0a
Sara Marcely Andrade de Oliveira <saradomacario@gmail.com>
2025-06-06 22:31:32 -0300

    PAM - Postagens // criação da view, viewmodels, e models

---
+++ Postagens/ViewModels/PostViewModels.cs
@@ -0,0 +1,15 @@
+namespace Postagens.ViewModels;
+
+public class PostViewModels : ContentPage
+{
+	public PostViewModels()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
